2a7487f53ed16a4c409e5f35590a97e25b98b107
vn-unitbv <norbert.vitalyos@student.unitbv.ro>
2023-12-09 17:57:31 +0200

    removed GameConfig.cpp

--- Main/SFML/src/GameConfig.h
+++ Main/SFML/src/GameConfig.h
@@ -5,8 +5,8 @@
 
 namespace GameConfig
 {
-    extern const std::string WINDOW_NAME;
-    extern const int FRAME_LIMIT;
-    extern const std::string FONT_PATH;
-    extern const std::array<std::string, 3> OPTIONS;
-};
+    inline const std::string WINDOW_NAME = "Battle City";
+    inline const int FRAME_LIMIT = 60;
+    inline const std::string FONT_PATH = "sfml_assets/fonts/joystix monospace.ttf";
+    inline const std::array<std::string, 3> OPTIONS = { "1 player", "2 players", "Exit" };
+}
